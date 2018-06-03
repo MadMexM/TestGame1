ced897a35d8356ad257c629b9b9e987e2649a9bc
MadMexM <mjsalinas1@gmail.com>
2018-06-03 15:01:04 -0500

    Main Class v1

--- src/com/testGame1/main/Main.java
+++ src/com/testGame1/main/Main.java
@@ -1,11 +1,26 @@
 package com.testGame1.main;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class Main {
 
-	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JFrame frame = new JFrame("Test Game");
 
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame.addWindowListener(new java.awt.event.WindowAdapter() {
+			@Override
+			public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+				if (JOptionPane.showConfirmDialog(frame, "Are you sure to exit the game?", "Exit Game?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+					System.exit(0);
+				}
+			}
+		});
+
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+		frame.setResizable(false);
 	}
 
 }
